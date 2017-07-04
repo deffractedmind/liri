88695120d466e3eef191b797201c327b6c1bda1b
Albert Factora <afactora@archimedes.local>
2017-07-04 11:32:16 -0700

    Initial commit

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'td46PpXRUG6kWkx14ue1GjjD7',
+  consumer_secret: 'KFxGkfLyZUdOahBnr9ceIJn48H4do5VCZ0Bf0N3YMKV7cnX7tO',
+  access_token_key: '2492204504-uoZnhsrvhPV8NteEAWEMXxr8YkqHdvHwWFqBfBE',
+  access_token_secret: '0R3iptika34w8rVdeT4w5C4Va34pUtwORdUEvHlYc7c9p',
+}
