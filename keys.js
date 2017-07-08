fc819debd3f694e9abffda039a4b9424bd7fe436
Albert Factora <afactora@calvisitor-10-105-176-117.calvisitor.1918.berkeley.edu>
2017-07-08 09:38:58 -0700

    Fixed twitter key reference

--- keys.js
+++ keys.js
@@ -1,6 +1,8 @@
-exports.twitterKeys = {
+var Twitter = require('twitter');
+
+exports.twitterKeys = new Twitter({
   consumer_key: 'td46PpXRUG6kWkx14ue1GjjD7',
   consumer_secret: 'KFxGkfLyZUdOahBnr9ceIJn48H4do5VCZ0Bf0N3YMKV7cnX7tO',
   access_token_key: '2492204504-uoZnhsrvhPV8NteEAWEMXxr8YkqHdvHwWFqBfBE',
-  access_token_secret: '0R3iptika34w8rVdeT4w5C4Va34pUtwORdUEvHlYc7c9p',
-}
+  access_token_secret: '0R3iptika34w8rVdeT4w5C4Va34pUtwORdUEvHlYc7c9p'
+});
--- liri.js
+++ liri.js
@@ -1,20 +1,41 @@
-var fs = require('fs');
-
-fs.readFile("keys.js", "utf8", function(error, data) {
-  if (error){
-    return console.log(error);
-  }
-  // var dataArr = data.split(",");
-  // console.log(data.consumer_key);
-
-  // var jsn = JSON.stringify(data)
-  // console.log(jsn);
-});
-
 var request = require("request");
 var arg = process.argv[2];
-// console.log(process.argv.length);
+
+
+
 if (arg === 'my-tweets') {
+  // var fs = require('fs');
+
+  // fs.readFile("keys.js", "utf8", function(error, data) {
+  //   if (error){
+  //     return console.log(error);
+  //   }
+    // var dataArr = data.split(",");
+    // console.log(data.consumer_key);
+
+    // var jsn = JSON.stringify(data)
+    // console.log(jsn);
+
+const key = require('./keys.js');
+
+  // });
+
+  // var Twitter = require('twitter');
+  //
+  // var client = new Twitter({
+  //   consumer_key: 'td46PpXRUG6kWkx14ue1GjjD7',
+  //   consumer_secret: 'KFxGkfLyZUdOahBnr9ceIJn48H4do5VCZ0Bf0N3YMKV7cnX7tO',
+  //   access_token_key: '2492204504-uoZnhsrvhPV8NteEAWEMXxr8YkqHdvHwWFqBfBE',
+  //   access_token_secret: '0R3iptika34w8rVdeT4w5C4Va34pUtwORdUEvHlYc7c9p'
+  // });
+
+  var params = {screen_name: 'veloxero'};
+  key.twitterKeys.get('statuses/user_timeline', params, function(error, tweets, response) {
+    if (!error) {
+      // console.log(tweets);
+    }
+    console.log(tweets[0].text);
+  });
 
 }
 else if (arg === 'spotify-this-song') {
@@ -32,8 +53,9 @@ else if (arg === 'spotify-this-song') {
     secret: '053a3b94963340acb0f87c4fbda41e54'
   });
 
+  log
+
 // 0hrBpAOgrt8RXigk83LLNE
-  // spotify.search({ type: 'track', query: songName }, function(err, data) {
   spotify.search({ type: 'track', query: songName }, function(err, data) {
     if (err) {
       return console.log('Error occurred: ' + err);
@@ -45,12 +67,11 @@ else if (arg === 'spotify-this-song') {
       console.log("Preview: " + data.tracks.items[4].preview_url);
     }
     else {
-      // console.log(JSON.stringify(data));
       var artistArr = [];
       for (var i=0; i<data.tracks.items[0].artists.length; i++) {
         artistArr.push(data.tracks.items[0].artists[i].name);
       }
-      console.log("Artist(s): " + artistArr.join(", "));//data.tracks.items[0].artists[1].name);
+      console.log("Artist(s): " + artistArr.join(", "));
       console.log("Album: " + data.tracks.items[0].name);
       console.log("Song: " + data.tracks.items[0].album.name);
       console.log("Preview: " + data.tracks.items[0].preview_url);
@@ -98,17 +119,19 @@ else if (arg === 'movie-this') {
 }
 else if (arg === 'do-what-it-says') {
   var fs = require('fs');
-
-  fs.readFile("random.txt", "utf8", function(error, data) {
-    if (error){
-      return console.log(error);
-    }
-    var dataArr = data.split(",");
-    var arg1 = process.argv[0];
-    var arg2 = process.argv[1]
-    var arg3 = dataArr[0];
-    var arg4 = dataArr[1]
-    console.log(arg1 + " " + arg2 + " " + arg3 + " " + arg4);
-
+    fs.readFile("random.txt", "utf8", function(error, data) {
+      if (error){
+        return console.log(error);
+      }
+      var dataArr = data.split(",");
+      var arg1 = process.argv[0];
+      var arg2 = process.argv[1]
+      var arg3 = dataArr[0];
+      var arg4 = dataArr[1]
+      console.log(arg1 + " " + arg2 + " " + arg3 + " " + arg4);
   });
+}
+else {
+  console.log('usage: node liri.js [my-tweets] | spotify-this-song "string"| movie-this "string"| do-what-it-says "string"]');
+  // console.log(process.env);
 }
